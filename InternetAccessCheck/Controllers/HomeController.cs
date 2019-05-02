2091e4808dedffa4bbdaf27db91942e57c354ad9
DORUK AKINCI (IT) <doruk.akinci@digiturk.com.tr>
2019-05-02 10:04:56 +0300

    initial commit

---
+++ InternetAccessCheck/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternetAccessCheck.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Stop Caching in IE
+            Response.Cache.SetCacheability(System.Web.HttpCacheability.NoCache);
+
+            // Stop Caching in Firefox
+            Response.Cache.SetNoStore();
+            return View();
+        }
+
+        public string HealthCheck()
+        {
+            // Stop Caching in IE
+            Response.Cache.SetCacheability(System.Web.HttpCacheability.NoCache);
+
+            // Stop Caching in Firefox
+            Response.Cache.SetNoStore();
+            return DateTime.Now.ToLocalTime().ToString();
+        }
+    }
+}
---
+++ InternetAccessCheck/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InternetAccessCheck.MvcApplication" Language="C#" %>
